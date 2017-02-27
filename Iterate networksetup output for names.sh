80c2386934c8541a942cab2f5740602efe722edc
eholtam <eholtam@mac.com>
2017-02-27 09:27:14 -0600

    Proper IFS handling

--- Iterate networksetup output for names.sh
+++ Iterate networksetup output for names.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-oldIFS="$IFS"
 IFS=$'\n'
 
 array=($(networksetup -listallhardwareports | awk '/^Hardware Port:/{gsub("^Hardware Port: ","");print}'))
@@ -10,7 +9,7 @@ networksetup -setwebproxystate "${interface}" off
 networksetup -setsecurewebproxystate "${interface}" off
 done
 
-IFS="${oldIFS}"
+unset IFS
 
 
 
